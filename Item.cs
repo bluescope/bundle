6942799dd78324eb89328a58b57ff824da99578a
Kushal <hada.kushal@gmail.com>
2013-03-21 01:58:03 -0500

    Create Item.cs
    create item

---
+++ Item.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bundle
+{
+    public class Item 
+    { 
+        public string MyProperty { get; set; }
+     }
+}
