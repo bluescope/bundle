d3402453f3499e325fb96ce43795ffc9255f4cfe
Kushal <hada.kushal@gmail.com>
2013-03-20 20:32:38 -0500

    Update Program.cs
    main function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 using System;
+
+namespace Panel_Bundling
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the input string: ");
+            var userInput = Console.ReadLine();
+            new Bundler(userInput);
+        }
+    }
+}
